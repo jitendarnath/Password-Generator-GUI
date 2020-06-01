300c5b4e9dde4523956c9cddc847fab0f9ee6db3
jitendarnath <jnb19012k@gmail.com>
2020-06-01 13:22:54 +0530

    clean fields for inputs

--- PassGen.py
+++ PassGen.py
@@ -2,6 +2,7 @@ import string
 from random import randint,choice
 from tkinter import *
 import tkinter.messagebox as tmsg
+from tkinter import filedialog
 
 #function for generating password
 def pass_gen():
@@ -12,17 +13,29 @@ def pass_gen():
 
 def save_pass():
     msg=tmsg.askquestion("Save As","Do you want to save?")
-    with open("D:\\Programming\\Python\\Project\\records.txt","a") as f:
+    with open("D:\\Programming\\Tkinter\\records.txt","a") as f:
         f.write(f"\n{account_name.get(), username.get(), password_entry.get()}")
         f.close()
     print("Saved Changes ..!!")
 
+    password_entry.delete(0,END)
+    account_name.delete(0,END)
+    username.delete(0,END)
+    
+
 def records():
-    top= Toplevel()
+    top= Tk()
     top.title('Records')
-    top.geometry("850x500")
+    # top.geometry("850x500")
     top.iconbitmap("Images/logo.ico")
-    top.config(background='#002447')
+    # top.config(background='#002447')
+    top.fileName = filedialog.askopenfilename \
+                (filetypes=(("Python Stuff", ".txt"), \
+                            ("All files","*.*"))) 
+    text1 = open(top.fileName).read()
+    T = Text(top, height=15, width=60)
+    T.pack()                 
+    T.insert(END,text1) 
     
 #Defining the Window
 window = Tk()
@@ -70,11 +83,11 @@ right_sub_frame1.pack(pady=15)
 right_sub_frame2= LabelFrame(right_frame,text=" Store Password : ",font=("Helvetica",14), bg='#002447',fg='light blue',padx=15,pady=10)
 
 account_name = Entry(right_sub_frame2,font=("Helvetica",15),bg='#002447',fg='PeachPuff')
-account_name.grid(row=0,columnspan=2,sticky="we",pady=5,ipadx=22,ipady=3)
+account_name.grid(row=0,columnspan=2,sticky="we",pady=5,ipadx=28,ipady=3)
 account_name.insert(0," Enter account name")
 
 username = Entry(right_sub_frame2,font=("Helvetica",15),bg='#002447',fg='PeachPuff')
-username.grid(row=1,column=0,pady=5,ipadx=22,ipady=3)
+username.grid(row=1,column=0,pady=5,ipadx=28,ipady=3)
 username.insert(0," Enter username")
 
 save_pw_button = Button(right_sub_frame2,text="Save",font=("Helvetica",12),bg='white',fg='#002447',command=save_pass,relief=RAISED)
@@ -96,7 +109,7 @@ m1 = Menu(main_menu,tearoff=0)
 m1.add_command(label="New",command=pass_gen)
 m1.add_command(label="Save", command=save_pass)
 m1.add_separator()
-m1.add_command(label="Save As", command=save_pass)
+m1.add_command(label="Open", command=save_pass)
 m1.add_command(label="Quit", command=window.quit)
 main_menu.add_cascade(label="Menu",menu=m1)
 window.config(menu=main_menu)
